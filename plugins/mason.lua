0a17fd99cc5bc86a0e6a9494bdabc20696f9b643
Paul Rawcliffe <jpr999@gmail.com>
2023-05-11 23:58:07 +0100

    Latest

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,11 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
         "bashls",
         "clangd",
         "cmake",
-        "lua_ls",
+        "luals",
       })
     end,
   },
